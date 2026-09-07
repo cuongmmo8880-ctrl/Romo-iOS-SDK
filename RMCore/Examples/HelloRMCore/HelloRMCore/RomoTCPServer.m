bc2ac50f5f144e17d0b99b2e89ed65f7c961de6b
cuongmmo8880-ctrl <cuongmmo8880@gmail.com>
2026-09-07 20:15:07 +0700

    Fix native Romo turn control for LEFT RIGHT

--- RMCore/Examples/HelloRMCore/HelloRMCore/RomoTCPServer.m
+++ RMCore/Examples/HelloRMCore/HelloRMCore/RomoTCPServer.m
@@ -229,11 +229,11 @@ static const int kRomoTCPPort = 5000;
 
     } else if ([upper isEqualToString:@"LEFT"]) {
 
-        [romo driveWithLeftMotorPower:-0.5 rightMotorPower:0.5];
+        [romo driveWithRadius:0.0 speed:0.3];
 
     } else if ([upper isEqualToString:@"RIGHT"]) {
 
-        [romo driveWithLeftMotorPower:0.5 rightMotorPower:-0.5];
+        [romo driveWithRadius:0.0 speed:-0.3];
 
     } else if ([upper isEqualToString:@"TILT_UP"]) {
 
